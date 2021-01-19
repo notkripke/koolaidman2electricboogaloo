7bd43f2285253aabb1ae83e90e6f612cda7b5a93
notkripke <75053648+notkripke@users.noreply.github.com>
2021-01-19 15:04:46 -0500

    Several changes
    This code was changed greatly during the meeting on 1/17/21. It now works perfectly in allowing the robot to drive with full range of movement and slow down while holding left trigger.

--- teamcode/Teleop/VanillaDrive.java
+++ teamcode/Teleop/VanillaDrive.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Teleop;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -15,20 +16,14 @@ public class VanillaDrive extends GorillabotsCentral {
     @Override
     public void runOpMode() {
 
+
         initializeComponents();
-
-        waitForStart();
-
-        ElapsedTime SlowTimer = new ElapsedTime(); //creates timer to prevent rapid stage increase
-
-        int slow = 0;
-
-        boolean DriveSlow = false;
-        boolean DriveSlowWatch = false;
 
         double x = 0;
         double r = 0;
         double y = 0;
+
+        waitForStart();
 
         while (opModeIsActive()) {
 
@@ -38,31 +33,13 @@ public class VanillaDrive extends GorillabotsCentral {
             y = -gamepad1.left_stick_y;
             r = gamepad1.right_stick_x;
 
-            if(gamepad1.b && SlowTimer.time() > 1.5)
-                slow += 1;
-            SlowTimer.reset();
-        }
-
-        telemetry.addData("Slow Driving Timer (< 1.5 sec to change)", SlowTimer);
-        telemetry.update();
-
-        switch (slow) {
-
-            case 0:
-                drive.go(x, y, r); // drive speed max
-
-                telemetry.addData("Driving slow?", "No");
-                telemetry.update();
-                break;
-            case 1:
+            if(gamepad1.left_trigger > 0.3){
                 drive.go(x * 0.25, y * 0.25, r * 0.25);
+            }
+            else {
+                drive.go(x * 0.85, y * 0.85, r * 0.85);
+            }
 
-                telemetry.addData("Driving slow?", "Yes");
-                telemetry.update();
-                break;
-            case 2: //for looping
-                slow = 0;
-                break;
         }
     }
 }
